5314b6fdcd21e97bdb76b5109aa7b80273ffe4b9
Antoine Rozo <antoine.rozo@gmail.com>
2025-11-25 01:05:48 +0100

    Private attributes using contextvars

--- private.py
+++ private.py
@@ -1,35 +1,52 @@
 import contextvars
-import gc
+import functools
 import inspect
 import weakref
+import types
 
 
 class Private:
     def __init__(self):
-        self._values = weakref.WeakKeyDictionary()
+        self._var = contextvars.ContextVar('_var')
+
+    def __set_name__(self, owner, name):
+        self.name = name
+        context = contextvars.copy_context()
+        old_getattribute = owner.__getattribute__
+
+        def get_patched(method):
+            @functools.wraps(method)
+            def patched(*args, **kwargs):
+                def wrapper():
+                    self._var.set(self._var.get(weakref.WeakKeyDictionary()))
+                    return method(*args, **kwargs)
+                return context.run(wrapper)
+            return patched
+
+        def getattribute(self, name):
+            attr = old_getattribute(self, name)
+            if not isinstance(attr, types.MethodType):
+                return attr
+
+            return get_patched(attr)
+
+        owner.__getattribute__ = getattribute
+        old_init = owner.__init__
+        owner.__init__ = get_patched(old_init)
 
     def __get__(self, instance, owner):
         if instance is None:
-            return '<private>'
-        frame = inspect.currentframe()
-        caller_frame = frame.f_back
-        if not caller_frame:
-            raise ValueError
-        caller_method = getattr(owner, caller_frame.f_code.co_name, None)
-        if caller_method is None or caller_method.__code__ is not caller_frame.f_code:
-            raise ValueError
-        return self._values[instance]
+            return self
+        try:
+            return self._var.get()[instance]
+        except (LookupError, KeyError):
+            raise AttributeError(self.name) from None
 
     def __set__(self, instance, value):
-        frame = inspect.currentframe()
-        caller_frame = frame.f_back
-        if not caller_frame:
-            raise ValueError
-        owner = type(instance)
-        caller_method = getattr(owner, caller_frame.f_code.co_name, None)
-        if caller_method is None or caller_method.__code__ is not caller_frame.f_code:
-            raise ValueError
-        self._values[instance] = value
+        try:
+            self._var.get()[instance] = value
+        except LookupError:
+            raise AttributeError(self.name) from None
 
 
 class Class:
@@ -43,6 +60,10 @@ class Class:
 
 
 obj = Class(5)
+obj2 = Class(1)
 print(obj.get_x())
+print(obj2.get_x())
+#print(Class.__dict__['x']._values[obj])
+#obj.x = 0
 #print(obj.x)
-print(Class.__dict__['x']._values[obj])
+#print(dict(Class.x._var.get()))
